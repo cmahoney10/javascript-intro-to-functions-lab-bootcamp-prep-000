e7e49f221945b90e6205bbe20c9c97b60a4418dc
Colette Mahoney <colettemahoney@hotmail.com>
2018-08-12 10:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === 'hello') {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
+  else if (string === 'HELLO') {
+    return "YES INDEED!";
+  }
+  else if (string === )
   
 }
